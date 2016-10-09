8167b221173ea9ddff8dd169ce83ac3a332e4049
Usman Sheikh <sh.usmanikram@hotmail.com>
2016-10-09 17:50:36 +0500

    Initial
    Students and user managment added

---
+++ DemoDeployment/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoDeployment.Models
+{
+    public class Student
+    {
+       
+        public int StudentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        
+        [Required(ErrorMessage ="Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public string Phone { get; set; }
+
+        [RegularExpression("^[0-9+]{5}-[0-9+]{7}-[0-9]{1}$")]
+        public string CNIC { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ DemoDeployment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoDeployment.Startup))]
+namespace DemoDeployment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
